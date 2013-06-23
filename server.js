9bd83a389eefa09843bca5ff1b6548efd5e622db
YuuuuuYuuuuu <yuu.android.dev@gmail.com>
2013-06-23 15:12:58 +0900

    modify server.js

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
-var sys = require('sys');
 var http = require('http');
-var server = http.createServer(
-function (request, response) {
-response.writeHead(200, {'Content-Type': 'text/plain'});
-response.write('Hello World!!\n');
-response.end();
-}
-).listen(3000);
-
-console.log("Listening on port 3000");
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Node Ninja\n');
+}).listen(80);
